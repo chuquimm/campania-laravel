2fd8d04fcd3d689d273ab5af1a052bd52d54802b
Marcos Chuquicondor <marcoschuquicondor@gmail.com>
2019-01-03 16:49:21 -0500

    Donantes Model/Migration/Controller

---
+++ database/migrations/2019_01_03_201925_create_donantes_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDonantesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('donantes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre');
+            $table->string('apellido');
+            $table->string('correo')->unique();
+            $table->timestamp('correo_verified_at')->nullable();
+            $table->string('dni', 8);
+            $table->string('celular', 11);
+            $table->date('fnacimiento');
+            $table->boolean('genero');
+            $table->unsignedInteger('sangre_id');
+            $table->char('lugarProcedencia_id', 6);
+            $table->longtext('foto');
+            $table->boolean('sms');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('donantes');
+    }
+}
