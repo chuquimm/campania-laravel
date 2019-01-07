5279e88528d12e7704bcd759b7bae18d8a88ebde
Marcos Chuquicondor <marcoschuquicondor@gmail.com>
2019-01-07 16:04:59 -0500

    Add row to DonantesTableSeeder

--- database/seeds/DonantesTableSeeder.php
+++ database/seeds/DonantesTableSeeder.php
@@ -16,9 +16,24 @@ class DonantesTableSeeder extends Seeder
         $donante->nombre='Marcos';
         $donante->apellido='Chuquicondor';
         $donante->correo='marcos@correo.com';
-        $donante->dni='70746679';
-        $donante->celular='969219171';
+        $donante->dni='70854469';
+        $donante->celular='968574142';
         $donante->fnacimiento=date("Y-m-d", strtotime("1998-03-23"));
+        $donante->genero='1';
+        $donante->tiposangre_id='7';
+        $donante->distrito_id='200101';
+        $donante->foto='avatar.png';
+        $donante->sms=0;
+        $donante->save();
+
+
+        $donante = new Donante;
+        $donante->nombre='Eduardo';
+        $donante->apellido='Mejia';
+        $donante->correo='mejia@correo.com';
+        $donante->dni='74589632';
+        $donante->celular='362514785';
+        $donante->fnacimiento=date("Y-m-d", strtotime("1997-09-30"));
         $donante->genero='1';
         $donante->tiposangre_id='7';
         $donante->distrito_id='200101';
