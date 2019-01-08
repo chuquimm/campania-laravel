42f1e86255098c88371246e99c834d7f401832ff
Marcos Chuquicondor <marcoschuquicondor@gmail.com>
2019-01-07 19:35:27 -0500

    UsersTableSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,11 +12,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
         $this->call(TiposangresTableSeeder::class);
         $this->call(DonantesTableSeeder::class);
         $this->call(CampaniasTableSeeder::class);
-        // $this->call(DistritosTableSeeder::class);
-        // $this->call(ProvinciasTableSeeder::class);
-        // $this->call(DepartamentosTableSeeder::class);
+        $this->call(DistritosTableSeeder::class);
+        $this->call(ProvinciasTableSeeder::class);
+        $this->call(DepartamentosTableSeeder::class);
     }
 }
